b243a8a9e5386a1dd7cce4136602fd64ef017be7
chanran <t14779760636@163.com>
2022-05-01 21:33:43 +0800

    master first commit

---
+++ 1_GitDemo/src/main/java/TestGit.java
@@ -0,0 +1,16 @@
+/**
+ * ClassName:TestGit
+ * Package:PACKAGE_NAME
+ * Description:
+ * date:2022/5/1 21:32
+ * author:chenran
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git Git");
+        System.out.println("hello Git Git");
+        System.out.println("hello Git Git");
+        System.out.println("hello Git Git");
+        System.out.println("hello Git Git");
+    }
+}
