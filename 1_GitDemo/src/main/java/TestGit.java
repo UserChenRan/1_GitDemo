c2fd6213b546157d5cacbb84b0b66b0be0681efc
chanran <t14779760636@163.com>
2022-05-01 21:43:27 +0800

    sub second commit

--- 1_GitDemo/src/main/java/TestGit.java
+++ 1_GitDemo/src/main/java/TestGit.java
@@ -9,7 +9,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello Git Git 2222");
         System.out.println("hello Git Git sub");
-        System.out.println("hello Git Git");
+        System.out.println("hello Git Git sub second");
         System.out.println("hello Git Git");
         System.out.println("hello Git Git");
     }
